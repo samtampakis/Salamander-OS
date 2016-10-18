68e0de8eac2356e2d413e5d97eba8f4f67b9b613
Sam Tampakis <sam.tampakis@gmail.com>
2016-10-18 01:53:13 -0400

    Initialized OP codes

--- distrib/host/cpu.js
+++ distrib/host/cpu.js
@@ -1,4 +1,5 @@
 ///<reference path="../globals.ts" />
+///<reference path="../os/userCommand.ts" />
 /* ------------
      CPU.ts
 
@@ -29,6 +30,7 @@ var TSOS;
             this.Yreg = Yreg;
             this.Zflag = Zflag;
             this.isExecuting = isExecuting;
+            this.opCodes = [];
         }
         Cpu.prototype.init = function () {
             this.PC = 0;
@@ -37,6 +39,48 @@ var TSOS;
             this.Yreg = 0;
             this.Zflag = 0;
             this.isExecuting = false;
+            var uc;
+            //
+            // Load the opCodes list.
+            // LDA
+            uc = new TSOS.UserCommand("A9");
+            this.opCodes[this.opCodes.length] = uc;
+            uc = new TSOS.UserCommand("AD");
+            this.opCodes[this.opCodes.length] = uc;
+            // STA
+            uc = new TSOS.UserCommand("8D");
+            this.opCodes[this.opCodes.length] = uc;
+            //ADC
+            uc = new TSOS.UserCommand("6D");
+            this.opCodes[this.opCodes.length] = uc;
+            //LDX
+            uc = new TSOS.UserCommand("A2");
+            this.opCodes[this.opCodes.length] = uc;
+            uc = new TSOS.UserCommand("AE");
+            this.opCodes[this.opCodes.length] = uc;
+            //LDY
+            uc = new TSOS.UserCommand("A0");
+            this.opCodes[this.opCodes.length] = uc;
+            uc = new TSOS.UserCommand("AC");
+            this.opCodes[this.opCodes.length] = uc;
+            //NOP
+            uc = new TSOS.UserCommand("EA");
+            this.opCodes[this.opCodes.length] = uc;
+            //BRK
+            uc = new TSOS.UserCommand("00");
+            this.opCodes[this.opCodes.length] = uc;
+            //CPX
+            uc = new TSOS.UserCommand("EC");
+            this.opCodes[this.opCodes.length] = uc;
+            //BNE        
+            uc = new TSOS.UserCommand("D0");
+            this.opCodes[this.opCodes.length] = uc;
+            //INC
+            uc = new TSOS.UserCommand("EE");
+            this.opCodes[this.opCodes.length] = uc;
+            //SYS
+            uc = new TSOS.UserCommand("FF");
+            this.opCodes[this.opCodes.length] = uc;
         };
         Cpu.prototype.cycle = function () {
             _Kernel.krnTrace('CPU cycle');
--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -1,4 +1,5 @@
 ///<reference path="../globals.ts" />
+///<reference path="../os/userCommand.ts" />
 
 /* ------------
      CPU.ts
@@ -18,6 +19,8 @@
 module TSOS {
 
     export class Cpu {
+        
+        public opCodes = [];
 
         constructor(public PC: number = 0,
                     public Acc: number = 0,
@@ -35,6 +38,63 @@ module TSOS {
             this.Yreg = 0;
             this.Zflag = 0;
             this.isExecuting = false;
+            
+            var uc;
+            //
+            // Load the opCodes list.
+
+            // LDA
+            uc = new UserCommand("A9");
+            this.opCodes[this.opCodes.length] = uc;
+            
+            uc = new UserCommand("AD");
+            this.opCodes[this.opCodes.length] = uc;            
+
+            // STA
+            uc = new UserCommand("8D");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //ADC
+            uc = new UserCommand("6D");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //LDX
+            uc = new UserCommand("A2");
+            this.opCodes[this.opCodes.length] = uc;
+            
+            uc = new UserCommand("AE");
+            this.opCodes[this.opCodes.length] = uc;            
+        
+            //LDY
+            uc = new UserCommand("A0");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            uc = new UserCommand("AC");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //NOP
+            uc = new UserCommand("EA");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //BRK
+            uc = new UserCommand("00");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //CPX
+            uc = new UserCommand("EC");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //BNE        
+            uc = new UserCommand("D0");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //INC
+            uc = new UserCommand("EE");
+            this.opCodes[this.opCodes.length] = uc;
+        
+            //SYS
+            uc = new UserCommand("FF");
+            this.opCodes[this.opCodes.length] = uc;
         }
 
         public cycle(): void {
