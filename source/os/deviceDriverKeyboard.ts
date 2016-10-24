49bc177c21b2ae11e1bd6dc12ec5da35fcaf99df
Sam Tampakis <sam.tampakis@gmail.com>
2016-10-23 22:39:26 -0400

    Special characters display properly. Relates to Issue #5

--- source/os/deviceDriverKeyboard.ts
+++ source/os/deviceDriverKeyboard.ts
@@ -54,10 +54,94 @@ module TSOS {
                         (keyCode == 8)                      ||   // backspace
                         (keyCode == 13)                     ||   // enter
                         (keyCode == 38)                     ||   // up arrow
-                        (keyCode == 40)){
+                        (keyCode == 40)){                        // down arrow
+                
+                // Convert Arrows into non-printable characters
+                if ((keyCode == 38) || (keyCode == 40)){
+                    keyCode -= 20;
+                    console.log(String.fromCharCode(keyCode));
+                }
+                
+                
+                //Convert digits to Special Characters if Shifted
+                if (isShifted){
+                    if(keyCode == 48){
+                        keyCode = 40;
+                    } else if (keyCode == 49){
+                        keyCode = 33;
+                    } else if (keyCode == 50){
+                        keyCode = 64;
+                    } else if ((keyCode >= 51) && (keyCode <= 53)){
+                        keyCode -= 16;
+                    } else if (keyCode == 54){
+                        keyCode = 94;
+                    } else if (keyCode == 55){
+                        keyCode = 38;
+                    } else if (keyCode == 56){
+                        keyCode = 42;
+                    } else if (keyCode == 57){
+                        keyCode = 41;
+                    }
+                    
+                    
+                    
+                }
+                
                 chr = String.fromCharCode(keyCode);
                 _KernelInputQueue.enqueue(chr);
-            } 
+            } else if (((keyCode >= 186) && (keyCode <= 192)) || 
+                       ((keyCode >= 219) && (keyCode <= 222))) {
+
+                //Special Characters
+                //Convert keyCode to charCode
+                
+                if(isShifted){
+                 
+                    //Characters that require shift key
+                    
+                    if(keyCode == 186){
+                        keyCode = 58;
+                    } else if (keyCode == 187){
+                        keyCode = 43;
+                    } else if (keyCode == 188){
+                        keyCode = 60;
+                    } else if (keyCode == 189){
+                        keyCode = 95;
+                    } else if (keyCode == 190){
+                        keyCode = 62;
+                    } else if (keyCode == 191){
+                        keyCode = 63;
+                    } else if (keyCode == 192){
+                        keyCode = 126;
+                    } else if ((keyCode >= 219) && (keyCode <= 221)){
+                        keyCode -= 96;
+                    } else if (keyCode == 222){
+                        keyCode = 34;
+                    }
+                    
+                } else{
+                    
+                    //Characters that don't require shift key
+                                
+                    if(keyCode == 186){
+                        keyCode = 59;
+                    } else if (keyCode == 187){
+                        keyCode = 61;
+                    } else if ((keyCode >= 188) && (keyCode <= 191)){
+                        keyCode -= 144;
+                    } else if (keyCode == 192){
+                        keyCode = 96;
+                    } else if ((keyCode >= 219) && (keyCode <= 221)){
+                        keyCode -= 128;
+                    } else if (keyCode == 222){
+                        keyCode = 39;
+                    }
+                
+                }
+
+                chr = String.fromCharCode(keyCode);
+                _KernelInputQueue.enqueue(chr);
+            }
         }
     }
 }
